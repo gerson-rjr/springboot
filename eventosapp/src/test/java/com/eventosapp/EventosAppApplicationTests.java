81797d7bb76f31fab19732ce7f62b7bdb0c2cbc3
Gerson Junior <gersonjunior2@outlook.com>
2023-05-17 23:44:49 -0300

    projeto de cadastro de eventos em spring boot

---
+++ eventosapp/src/test/java/com/eventosapp/EventosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
